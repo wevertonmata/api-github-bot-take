710ad0e9bf3abef3cf07d0fe091f3d85ae04d2fe
Weverton Mata <ryanmata40@gmail.com>
2020-09-15 22:24:57 -0300

    number #1 commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const fetch = require("node-fetch");
+
+const url = "https://api.github.com/users/takenet/repos?&page=1&per_page=10&sort=created&direction=asc" //Docs API Github
+let data = []
+
+app.get("/dados/:id", async (req, res) => {
+
+  fetch(url, { method: "Get" })
+    .then(res => res.json())
+      .then((json) => {
+        for(i=0;i<json.length;i++) {
+          let { full_name, description, language, created_at } = json[i]
+
+          if(language == 'C#'){ 
+            data.push({full_name, description, created_at})
+          }
+        }
+        res.status(200).send(data)   
+      });
+});
+
+
+app.listen(3000, () => {
+    console.log("Backend executando...")
+  });
+
+
